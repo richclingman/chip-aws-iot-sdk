7844cde787506da2051589689148724c9d6c02c5
Rich Clingman <richclingman@gmail.com>
2017-05-16 13:04:07 -0700

    set up for tests

---
+++ test/chipProDevTest.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const expect = require("chai").expect;
+const chipProDev = require('../driver/chipProDev');
+
+describe('chipProDev', function () {
+    it('should be true', function () {
+        expect(true).to.be.true;
+    });
+
+});
